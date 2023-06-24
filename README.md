f848c13ffd840f5769d6b08df88d002614c3d801
KM Mohsin <km.mohsin@signanthealth.com>
2023-06-23 21:29:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# LLM Based Question and Answering App
+
+
+This project is a simple question and answering app that utilizes state-of-the-art language models and tools such as OpenAI, Langchain, Llama_index, and Gradio. The app is designed to answer user’s questions based on a previously given document. With its advanced natural language processing capabilities, the app provides accurate and relevant answers to user queries
+
+## Environment Setup
+
+Before running this script make sure that you have the environment setup. I have tested it python3.10. 
+This script will use OpenAI API Key. So make sure you have the API Key ready and set it in the environment.
+The full environment snapshot is shared in **_full_environment.txt_** file.
+Packages: gradio, langchain, llama-index, tiktoken, openai, pypdf
+
+You can run this following command to install all the required packages.
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Make sure that you have OpenAI API key as a environment variable named as **_OPENAI_API_KEY_**
+
+```bash
+$ python -m main.py
+```
+
+## Author
+
+KM Mohsin (mohsin.eee@gmail.com) 
---
+++ main.py
@@ -0,0 +1,58 @@
+
+"""
+Please install these following libraries
+# tiktoken, openai, langchain, llama_index, pypdf, gradio
+# Have your OpenAI key ready and set in the Environment variable
+"""
+
+from llama_index import SimpleDirectoryReader  # Reading docs
+from llama_index import VectorStoreIndex  # for vector index management
+from llama_index import StorageContext  # for index storing locally
+from llama_index import load_index_from_storage  # to retrieve locally stored index
+import gradio as gr
+
+# os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY  # need to placed under env variables form run config
+DOC_PATH = './llama_docs'
+INDEX_PATH = './llama_index'
+
+
+def construct_index(doc_path=DOC_PATH, index_store=INDEX_PATH, use_cache=False):
+    """
+
+    :param doc_path: Relative path where document will be stored to be indexed
+    :param index_store: Relative path where index files will be stored
+    :param use_cache: if True then won't use previously generated index store
+    :return: None, index is locally stored in index_store location
+    """
+    if use_cache:
+        # rebuild storage context
+        storage_context = StorageContext.from_defaults(persist_dir=index_store)
+        index = load_index_from_storage(storage_context)  # load index
+    else:
+        documents = SimpleDirectoryReader(doc_path).load_data()
+        print(f"total_doc/pages found in the directory: {len(documents)}")
+        print("Generating index...")
+        index = VectorStoreIndex.from_documents(documents)
+        index.storage_context.persist(persist_dir=index_store)
+    return None
+
+
+def qabot(input_text):
+    """
+
+    :param input_text: input prompt
+    :return: response to the prompt
+    """
+    storage_context = StorageContext.from_defaults(persist_dir="./llama_index")
+    index = load_index_from_storage(storage_context)
+    query_engine = index.as_query_engine()
+    response = query_engine.query(input_text)
+    return response.response
+
+
+if __name__ == "__main__":
+    construct_index(DOC_PATH, use_cache=False)
+    iface = gr.Interface(fn=qabot, inputs=gr.inputs.Textbox(lines=7, label='Enter your query'),
+                         outputs="text",
+                         title="Question and Answering App")
+    iface.launch(share=True)
